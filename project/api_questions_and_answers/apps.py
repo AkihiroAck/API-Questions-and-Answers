279393a0cd9d1179128a4fb99d7f7a0646f0b9a5
AkihiroAck <canpolad99@gmail.com>
2025-09-02 22:40:32 +0500

    2025-09-02

---
+++ project/api_questions_and_answers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiQuestionsAndAnswersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_questions_and_answers'
